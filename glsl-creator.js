3f60a7ee787f36df9ad2d3aa25f8cddc120f5d0a
Matthew Jee <matthew@marmphco.com>
2015-03-26 22:05:04 -0700

    this doesn't work

--- glsl-creator.js
+++ glsl-creator.js
@@ -1,14 +1,52 @@
 var GLSLCreator = (function(exports) {
 
+   var uniformSuffix = function(type) {
+      switch (type) {
+         case gl.FLOAT:      return "1f";
+         case gl.FLOAT_VEC2: return "2f";
+         case gl.FLOAT_VEC3: return "3f";
+         case gl.FLOAT_VEC4: return "4f";
+         case gl.FLOAT_MAT2: return "Matrix2fv";
+         case gl.FLOAT_MAT3: return "Matrix3fv";
+         case gl.FLOAT_MAT4: return "Matrix4fv";
+         case gl.INT: return "1i";
+         case gl.INT_VEC2: return "2i";
+         case gl.INT_VEC3: return "3i";
+         case gl.INT_VEC4: return "4i";
+         case gl.BOOL: return "1i";
+         case gl.BOOL_VEC2: return "2i";
+         case gl.BOOL_VEC3: return "3i";
+         case gl.BOOL_VEC4: return "4i";
+         case gl.SAMPLER_2D: return "1i";
+         case gl.SAMPLER_CUBE: return "1i";
+      }
+   }
+
    var Context = function(element) {
       var gl = this.gl = element.getContext("webgl");
-      console.log(gl.drawingBufferWidth);
-      console.log(gl.canvas.height);
-
-      gl.clearColor(0.0, 1.0, 0.0, 1.0)
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
    }
+
+   /*
+
+   gl.FLOAT,
+gl.FLOAT_VEC2,
+gl.FLOAT_VEC3,
+gl.FLOAT_VEC4,
+gl.FLOAT_MAT2,
+gl.FLOAT_MAT3,
+gl.FLOAT_MAT3,
+gl.FLOAT_MAT4,
+gl.INT,
+gl.INT_VEC2,
+gl.INT_VEC3,
+gl.INT_VEC4,
+gl.BOOL,
+gl.BOOL_VEC2,
+gl.BOOL_VEC3,
+gl.BOOL_VEC4,
+gl.SAMPLER_2D
+gl.SAMPLER_CUBE
+   */
 
    /* Mesh */
    var Mesh = function(gl, vertices, indices, drawMode) {
@@ -38,32 +76,29 @@ var GLSLCreator = (function(exports) {
       }
    }
 
-   /* PortType */
+   /* PortTypes that have no OpenGL analog */
    var PortType = {
-      "Number": "NumberPortType",
       "String": "StringPortType",
       "FragmentShader": "FragmentShaderPortType",
       "VertexShader": "VertexShaderPortType",
       "ShaderProgram": "ProgramPortType",
-      "Texture": "TexturePortType",
       "Mesh": "MeshPortType"
    };
 
    /* InputPort */
 
-   var InputPort = function(node, name, type) {
+   var InputPort = function(node, type) {
       var _node = node;
-      var _name = name;
       var _type = type;
       var _provider = null;
       var self = this;
 
-      this.name = function() {
-         return _name;
-      }
-
       this.type = function() {
          return _type;
+      }
+
+      this.setType = function(type) {
+         _type = type;
       }
 
       this.hasProvider = function() {
@@ -118,16 +153,11 @@ var GLSLCreator = (function(exports) {
 
    /* OutputPort */
 
-   var OutputPort = function(node, name, type) {
+   var OutputPort = function(node, type) {
       var _node = node;
-      var _name = name;
       var _type = type;
       var _value;
       var _receivers = [];
-
-      this.name = function() {
-         return _name;
-      }
 
       this.type = function() {
          return _type;
@@ -158,12 +188,12 @@ var GLSLCreator = (function(exports) {
    /* Node */
 
    var Node = function() {
-      this.inputPorts = function() {
-         return this._inputPorts;
+      this.inputPort = function(name) {
+         return this._inputPorts[name];
       }
 
-      this.outputPorts = function() {
-         return this._outputPorts;
+      this.outputPort = function(name) {
+         return this._outputPorts[name];
       }
 
       this.markDirty = function() {
@@ -180,12 +210,16 @@ var GLSLCreator = (function(exports) {
       }
    }
 
+   /* ValueNode */
+
    var ValueNode = function(type) {
-      var valuePort = new OutputPort(this, "value", type);
+      var valuePort = new OutputPort(this, type);
 
       this._dirty = false;
-      this._inputPorts = [];
-      this._outputPorts = [valuePort];
+      this._inputPorts = {};
+      this._outputPorts = {
+         "value": valuePort
+      };
 
       this.setValue = function(value) {
          valuePort.exportValue(value);
@@ -195,65 +229,67 @@ var GLSLCreator = (function(exports) {
    }
    ValueNode.prototype = new Node();
 
-   var RenderNode = function(gl) {
-      var meshPort = new InputPort(this, "mesh", PortType.Mesh);
-      var programPort = new InputPort(this, "program", PortType.Program);
+   /* ImageNode */
+
+   var ImageNode = function(gl) {
+      var texturePort = new OutputPort(this, gl.SAMPLER_2D);
 
       this._dirty = false;
-      this._inputPorts = [meshPort, programPort];
-      this._outputPorts = [/* texture */];
+      this._inputPorts = {};
+      this._outputPorts = {
+         "texture": texturePort
+      };
 
-      this.evaluate = function() {
-         var mesh = meshPort.value();
-         var program = programPort.value();
-         gl.useProgram(program);
-         mesh.use();
-
-         // set up attributes and uniforms
-         //cheating
-         var positionLoc = gl.getAttribLocation(program, "iPosition");
-         gl.enableVertexAttribArray(positionLoc);
-         gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 12, 0);
-
-         // draw to main renderbuffer for now
-         mesh.draw();
-
+      var _texture = gl.createTexture(); // needs to be cleaned up
+      this.setImageData = function(imageData) {
+         gl.activeTexture(gl.TEXTURE0);
+         gl.bindTexture(gl.TEXTURE_2D, _texture);
+         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+         gl.bindTexture(gl.TEXTURE_2D, null);
+         texturePort.exportValue(_texture);
       }
-   }
-   RenderNode.prototype = new Node();
 
-   var Texture = function() {
-
+      this.evaluate = function() {}
    }
+   ImageNode.prototype = new Node();
 
    /* MeshNode */
+
    var MeshNode = function(gl, vertices, indices, drawMode) {
-      var meshPort = new OutputPort(this, "mesh", PortType.Mesh);
+      var meshPort = new OutputPort(this, PortType.Mesh);
       meshPort.exportValue(new Mesh(gl, vertices, indices, drawMode));
 
       this._dirty = false;
-      this._inputPorts = [];
-      this._outputPorts = [
-         meshPort,
-      ];
+      this._inputPorts = {};
+      this._outputPorts = {
+         "mesh": meshPort,
+      };
 
       this.evaluate = function() {}
    }
    MeshNode.prototype = new Node();
 
    /* ShaderNode */
+
    var ShaderNode = function(gl, type) {
-      var sourcePort = new InputPort(this, "source", PortType.String);
+      var sourcePort = new InputPort(this, PortType.String);
 
       var portType = type == gl.VERTEX_SHADER ?
          PortType.VertexShader : PortType.FragmentShader;
-      var shaderPort = new OutputPort(this, "shader", portType);
+      var shaderPort = new OutputPort(this, portType);
 
       var _shader = gl.createShader(type);
 
       this._dirty = false;
-      this._inputPorts = [sourcePort];
-      this._outputPorts = [shaderPort];
+      this._inputPorts = {
+         "source": sourcePort
+      };
+      this._outputPorts = {
+         "shader": shaderPort
+      };
 
 
       this.evaluate = function() {
@@ -269,15 +305,20 @@ var GLSLCreator = (function(exports) {
    /* ProgramNode */
 
    var ProgramNode = function(gl) {
-      var vertexShaderPort = new InputPort(this, "vertex shader", PortType.VertexShader);
-      var fragmentShaderPort = new InputPort(this, "fragment shader", PortType.FragmentShader);
-      var programPort = new OutputPort(this, "program", PortType.Program);
+      var vertexShaderPort = new InputPort(this, PortType.VertexShader);
+      var fragmentShaderPort = new InputPort(this, PortType.FragmentShader);
+      var programPort = new OutputPort(this, PortType.Program);
 
       var _program = gl.createProgram();
 
       this._dirty = false;
-      this._inputPorts = [vertexShaderPort, fragmentShaderPort];
-      this._outputPorts = [programPort];
+      this._inputPorts = {
+         "vertexShader": vertexShaderPort,
+         "fragmentShader": fragmentShaderPort
+      };
+      this._outputPorts = {
+         "program": programPort
+      };
 
       this.evaluate = function() {
          // remove attached shaders
@@ -290,19 +331,113 @@ var GLSLCreator = (function(exports) {
          gl.attachShader(_program, vertexShaderPort.value());
          gl.attachShader(_program, fragmentShaderPort.value());
 
-         // link
          gl.linkProgram(_program);
          console.log("Program Link Errors: " + gl.getProgramInfoLog(_program));
 
-         var x = gl.getProgramParameter(_program, gl.ACTIVE_ATTRIBUTES);
-         console.log(x);
-         console.log("active attributes:", gl.getActiveAttrib(_program, 0).name)
-
-         //export
          programPort.exportValue(_program);
       }
    }
    ProgramNode.prototype = new Node();
+
+   /* RenderNode */
+
+   var RenderNode = function(gl) {
+      var meshPort = new InputPort(this, PortType.Mesh);
+      var programPort = new InputPort(this, PortType.Program);
+
+      this._dirty = false;
+      this._inputPorts = {
+         "mesh": meshPort,
+         "program": programPort
+      };
+      this._outputPorts = {/* texture */};
+      this._uniformPorts = {};
+
+      var generateUniformPorts = function(program, self) {
+         // get uniform count
+         var uniformCount = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+
+         var ports = {};
+         for (var loc = 0; loc < uniformCount; loc++) {
+            var uniform = gl.getActiveUniform(program, loc);
+
+            // doesn't cover all the bases
+            // see https://www.opengl.org/sdk/docs/man/html/glGetActiveUniform.xhtml
+            // for full list
+
+            // merge ports
+            if (self._uniformPorts.hasOwnProperty(uniform.name)) {
+               console.log("reuse port " + uniform.name)
+               ports[uniform.name] = self._uniformPorts[uniform.name];
+               ports[uniform.name].setType(uniform.type);
+            }
+            else {
+               console.log("new port " + uniform.name);
+               ports[uniform.name] = new InputPort(self, uniform.type);
+            }
+
+            // clean up unused ports
+            for (var portName in self._uniformPorts) {
+               if (!ports.hasOwnProperty(portName)) {
+                  self._uniformPorts[portName].unbind();
+               }
+            }
+         }
+         return ports;
+      }
+
+      this.inputPort = function(name) {
+         return this._inputPorts[name] || this._uniformPorts[name];
+      }
+
+      this.evaluate = function() {
+         var mesh = meshPort.value();
+         var program = programPort.value();
+         gl.useProgram(program);
+         mesh.use();
+
+         // generate input ports for new uniforms
+         this._uniformPorts = generateUniformPorts(program, this);
+
+         // set up uniforms
+         var textureUnit = 0;
+         for (var name in this._uniformPorts) {
+            var loc = gl.getUniformLocation(program, name);
+            var port = this._uniformPorts[name];
+            var functionId = "uniform" + uniformSuffix(port.type());
+            var uniformFunc = gl[functionId];
+            // wont work for matrices and stuff
+            console.log(this._uniformPorts);
+            console.log(name)
+            console.log(port)
+            console.log(port.value())
+            console.log(uniformFunc);
+
+            if (port.type() == gl.SAMPLER_2D) {
+               uniformFunc.apply(gl, [loc, textureUnit]);
+
+               gl.activeTexture(gl["TEXTURE" + (textureUnit++).toString()]);
+               gl.bindTexture(gl.TEXTURE_2D, port.value());
+            }
+            else {
+               uniformFunc.apply(gl, [loc, port.value()]);
+            }
+         }
+
+         // hardcoded attributes
+         var positionLoc = gl.getAttribLocation(program, "iPosition");
+         gl.enableVertexAttribArray(positionLoc);
+         gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 12, 0);
+
+         // draw to main renderbuffer for now
+         gl.clearColor(0.0, 0.0, 0.0, 1.0)
+         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+         mesh.draw();
+
+      }
+   }
+   RenderNode.prototype = new Node();
 
    return {
       "PortType": PortType,
@@ -311,6 +446,7 @@ var GLSLCreator = (function(exports) {
       "OutputPort": OutputPort,
       "Node": Node,
       "ValueNode": ValueNode,
+      "ImageNode": ImageNode,
       "MeshNode": MeshNode,
       "ShaderNode": ShaderNode,
       "ProgramNode": ProgramNode,
